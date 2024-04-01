22d5b4110f0095963fbe84f0a1a387a0a742718f
Tyler Stambaugh <tyler.stambaugh@sevencorners.com>
2024-03-31 20:22:23 -0400

    getting 401'd after getting an auth token...

--- node_helper.js
+++ node_helper.js
@@ -8,59 +8,59 @@ const Log = require("logger");
 module.exports = NodeHelper.create({
 
   accessTokenData: {},
+  
   start: function () {
     console.log("Starting node_helper for: " + this.name);
   },
+
+
+
   getAccessToken: async function (payload) {
+    
     try{
-      console.log("Node helper getting access token: clientId:" + payload.clientId + " clientSecret: " + payload.clientSecret + " refreshToken: " + payload.refreshToken);
-      // var query = $"client_id={_config.ClientId}&client_secret={_config.ClientSecret}&refresh_token={_config.RefreshToken}&grant_type=refresh_token";
       url = payload.tokenUrl + "client_id=" + payload.clientId + "&client_secret=" + payload.clientSecret + "&refresh_token=" + payload.refreshToken + "&grant_type=refresh_token";
-      return axios.post(url)
+      await axios.post(url)
       .then(response => {
         // Handle the response data
-        console.log("Access token data: " + response.data);
-        accessTokenData = JSON.stringify(response.data);
+        Log.info("Access token data: " + JSON.stringify(response.data));
+        accessTokenData = response.data;
       })
       .catch(error => {
         // Handle errors
-        console.error('Error fetching data from API:', error);
+        console.error('access token Error fetching data from API:', error);
         return null;
       });
     } catch (error) {
-      console.error('Error fetching data from API:', error);
+      console.error('Access token Error fetching data from API:', error);
       return null;
     }
   },
 
   getStravaStats: async function (payload) {
+    //check if access token is expired, refresh if needed:
     try{
+      await this.getAccessToken(payload);
+      Log.info("node helper about to call for activities, access token: " + accessTokenData.access_token);
+      url = payload.url + "athlete/activities?before=" + payload.endTime + "&after=" + payload.startTime;
 
-      //$ http GET "https://www.strava.com/api/v3/athlete/activities?before=&after=&page=&per_page=" "Authorization: Bearer [[token]]"
-      console.log("Node helper getting Strava stats: clientId:" + payload.clientId + " clientSecret: " + payload.clientSecret + " refreshToken: " + payload.refreshToken);
-      await this.getAccessToken(payload).then((response) => {;
-      url = payload.url +
-      "athlete/activities?before=" +
-      payload.endTime +
-      "&after=" +
-      payload.startTime
-      return axios.get(url, {
+      await axios.get(url, {
         headers: {
           Authorization: `Bearer ${accessTokenData.access_token}`
         }
+      
       })
       .then(response => {
-        // Handle the response data
+        console.info("Node helper get Activities call - Data from API: ", response.data);
         return response.data;
       })
       .catch(error => {
         // Handle errors
-        console.error('Error fetching data from API:', error);
-        return null;
+        console.error('node helper getStravaStats (stringified) - Error fetching data from API:', JSON.stringify(error));
+        return error;
       });
-    });
+   
     } catch (error) {
-      console.error('Error fetching data from API:', error);
+      console.error('node helper getStravaStats - Error fetching data from API:', error);
       return null;
     }
   },
@@ -68,7 +68,7 @@ module.exports = NodeHelper.create({
 
 
   socketNotificationReceived: function (notification, payload) {
-    console.log("node helper received a socket notification: " + notification + " - Payload: " + payload);
+    console.error("node helper received a socket notification: " + notification + " - Payload: " + payload);
     if (notification === "GET_ACCESS_TOKEN") {
       this.getAccessToken(payload);
     } else if (notification === "GET_STRAVA_STATS") {
